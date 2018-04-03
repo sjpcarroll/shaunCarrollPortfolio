863634c1ea070be9dbcdaba8c79084a5bd1c507f
Shaun Carroll <sjpcarroll@gmail.com>
2018-04-03 19:37:00 +1000

    lazy entering of 3d box with css effects

---
+++ 3d-box-css-effects.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="3d animated boxs using CSS">
+    <meta name="author" content="Shaun Carroll">
+    <meta name="”robots”" content="”index," nofollow”/>
+    <meta name="geo.region" content="AU"/>
+    <meta name="geo.position" content="-25.274398;133.775136"/>
+    <meta name="ICBM" content="-25.274398, 133.775136"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>3D Boxes</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <!-- Bootstrap 4 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/3bBoxCssEffects.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+
+    <h1 class="text-center">Adding CSS effects to make a 3D menu</h1>
+    <hr>
+    <div class="row">
+        <div class="col">
+            <div class="outer">
+                <div class="inner">
+                    <img class="img-fluid front" src="https://ia.media-imdb.com/images/M/MV5BNDViYjAyZWUtNGQxMy00MDUyLTlkZTAtOWNkY2M5ZTk5MTE5XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_UY268_CR3,0,182,268_AL_.jpg">
+                    <div class="info">
+                        <header>
+                            <h1>The IT Crowd</h1>
+                            <span class="pub">1983 - 1993</span> <br>
+                            <span class="year">Stars: Patrick Stewart, Brent Spiner, Jonathan Frakes </span>
+                            <img class="img-fluid" src="http://i0.kym-cdn.com/entries/icons/mobile/000/000/554/picard-facepalm.jpg">
+                        </header>
+                        <hr>
+                        <p>Set decades after Captain Kirk's five-year mission, a new generation of Starfleet officers set off in a new Enterprise on their own mission to go where no one has gone before.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="outer">
+                <div class="inner">
+                    <img class="img-fluid front" src="https://ia.media-imdb.com/images/M/MV5BMTAxNTEyODE5MTNeQTJeQWpwZ15BbWU4MDE3MjM3ODQz._V1_UX182_CR0,0,182,268_AL_.jpg">
+                    <div class="info">
+                        <header>
+                            <h1>The IT Crowd</h1>
+                            <span class="pub">2014 - 2018</span> <br>
+                            <span class="year">Stars: Thomas Middleditch, T.J. Miller, Josh Brener </span>
+                            <img class="img-fluid" src="https://ia.media-imdb.com/images/M/MV5BMjI5MzE3ODkyMl5BMl5BanBnXkFtZTgwMDcxODQ1NDM@._V1_SY1000_CR0,0,1498,1000_AL_.jpg">
+                        </header>
+                        <hr>
+                        <p>Follows the struggle of Richard Hendricks, a Silicon Valley engineer trying to build his own company called Pied Piper.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="outer">
+                <div class="inner">
+                    <img class="img-fluid front" src="https://ia.media-imdb.com/images/M/MV5BMjE5MThjMzAtNWVmNC00YThkLTlmNzktMTM3Yzk4YTZhMTgwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY268_CR2,0,182,268_AL_.jpg">
+                    <div class="info">
+                        <header>
+                            <h1>The IT Crowd</h1>
+                            <span class="pub">2006 - 2013</span> <br>
+                            <span class="year">Stars:  Chris O'Dowd, Richard Ayoade, Katherine Parkinson</span>
+                            <img class="img-fluid" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/The_IT_Crowd_title_card.jpg/250px-The_IT_Crowd_title_card.jpg">
+                        </header>
+                        <hr>
+                        <p>The comedic adventures of a rag-tag group of technical support workers at a large corporation.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- @end container -->
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
